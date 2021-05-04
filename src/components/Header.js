6c937bf728213f17758976b113cff959f71e0953
hudyu17 <hengjeung.yuen@gmail.com>
2021-05-03 22:17:20 -0400

    insta link

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <header className='header'>
             <h1>work by the water 🏖️</h1>
-            <a href='https://www.instagram.com/' target='blank'>follow here!</a>
+            <a href='https://www.instagram.com/work.bythewater/' target='blank'>follow here!</a>
             <a href='https://www.buymeacoffee.com/hudyu17' target='blank'>support here!</a>
         </header>
     )
